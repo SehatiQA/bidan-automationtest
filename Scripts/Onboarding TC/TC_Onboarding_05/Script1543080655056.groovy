68fd6b039291593734ecd4aaad4089a4d3d4c78c
Sehati - Rudi <Sehati - Rudi@DESKTOP-VR4JD5J.local>
2018-12-12 12:25:07 +0700

    adding TC onboarding 2,3,4,5

---
+++ Scripts/Onboarding TC/TC_Onboarding_05/Script1543080655056.groovy
@@ -0,0 +1,45 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+'Buka aplikasi'
+Mobile.startApplication(GlobalVariable.apkname, true, FailureHandling.CONTINUE_ON_FAILURE)
+
+'Check tombol berikutnya tampil'
+Mobile.verifyElementExist(findTestObject('Object Repository/Onboarding/Tombol Berikutnya'), 0, FailureHandling.CONTINUE_ON_FAILURE)
+
+'Tap tombol berikutnya'
+Mobile.tap(findTestObject('Object Repository/Onboarding/Tombol Berikutnya'), 0, FailureHandling.CONTINUE_ON_FAILURE)
+
+'Check Overlay apakah sudah terdaftar di bidan sehati tampil'
+Mobile.verifyElementExist(findTestObject('Object Repository/Onboarding/Tombol Sudah'), 0, FailureHandling.CONTINUE_ON_FAILURE)
+
+'Tap tombol sudah'
+Mobile.tap(findTestObject('Onboarding/Tombol Sudah'), 0, FailureHandling.CONTINUE_ON_FAILURE)
+
+'wait'
+Mobile.delay(3, FailureHandling.CONTINUE_ON_FAILURE)
+
+'Check halaman login tampil'
+Mobile.verifyElementExist(findTestObject('Login/Tombol Masuk'), 0, FailureHandling.CONTINUE_ON_FAILURE)
+
+'close aplikasi'
+Mobile.closeApplication(FailureHandling.CONTINUE_ON_FAILURE)
+
